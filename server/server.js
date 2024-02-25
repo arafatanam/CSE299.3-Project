8a55d2f32aed4b4e2a708ad6ed35358a91f89f94
arafatanam <arafat.chowdhury05@northsouth.edu>
2024-02-25 11:31:24 +0600

    Updated Backend

--- server/server.js
+++ server/server.js
@@ -8,36 +8,44 @@ const cookieSession = require("cookie-session");
 const passportSetup = require("./passport");
 const app = express();
 const bodyParser = require("body-parser");
+const { google } = require('googleapis');
+const keys = require('./routes/keys.json')
 
-app.use(
-    cookieSession({
-        name: "session",
-        keys: ["cyberwolve"],
-        maxAge: 24 * 60 * 60 * 100,
-    })
-);
-
+app.use(cookieSession({name: "session", keys: ["cyberwolve"], maxAge: 24 * 60 * 60 * 100,}));
 app.use(passport.initialize());
 app.use(passport.session());
-
-app.use(
-    cors({
-        origin: "http://localhost:3000",
-        methods: "GET,POST,PUT,DELETE",
-        credentials: true,
-    })
-);
+app.use(cors({origin: "http://localhost:3000", methods: "GET,POST,PUT,DELETE", credentials: true,}));
 app.use(bodyParser.json());
-
 app.use("/auth", authRoute);
 app.use("/form-process", formProcessRoute);
 
-app.post("/getData", (req, res) => {
-    const { studentInfoLink, assessmentLink } = req.body;
-    console.log("Student Information Link:", studentInfoLink);
-    console.log("Assessment Link:", assessmentLink);
-    res.json({ studentInfoLink, assessmentLink });
-});
+function getSpreadsheetIdFromLink(assessmentLink) {
+    const url = new URL(assessmentLink);
+    const pathSegments = url.pathname.split('/');
+    if (pathSegments.length < 4 || pathSegments[1] !== "spreadsheets" || pathSegments[2] !== "d") {
+      throw new Error("Invalid assessment link format. Expected: https://docs.google.com/spreadsheets/d/<spreadsheet_id>");
+    }
+    return pathSegments[3];
+  }
+
+app.post("/getData", async (req, res) => {
+    try {
+      const { studentInfoLink, assessmentLink } = req.body;
+      const spreadsheetId = getSpreadsheetIdFromLink(assessmentLink);
+      const client = new google.auth.JWT(keys.client_email, null, keys.private_key, ["https://www.googleapis.com/auth/spreadsheets"]);
+      const opt = {spreadsheetId, range: "A2:A"};
+      await client.authorize(); 
+      const gsapi = google.sheets({ version: "v4", auth: client });
+      const data = await gsapi.spreadsheets.values.get(opt);
+      console.log(data.data.values);
+      res.json({ studentInfoLink, assessmentLink, data });
+    } 
+    catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Failed to process data" });
+    }
+  });
+
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
