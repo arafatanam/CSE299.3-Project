c2f465b1131500b31f5adf104c7eb3e3489268b5
arafatanam <arafat.chowdhury05@northsouth.edu>
2024-02-29 23:39:11 +0600

    Google Form Creation Integration

--- server/server.js
+++ server/server.js
@@ -8,7 +8,8 @@ const passportSetup = require("./passport");
 const app = express();
 const bodyParser = require("body-parser");
 const { google } = require('googleapis');
-const keys = require('./routes/keys.json')
+const keys = require('./routes/keys.json');
+const googleScriptUrl = 'https://script.google.com/macros/s/your_google_script_id/exec';
 
 app.use(cookieSession({ name: "session", keys: ["cyberwolve"], maxAge: 24 * 60 * 60 * 100, }));
 app.use(passport.initialize());
@@ -30,16 +31,17 @@ app.post("/getData", async (req, res) => {
   try {
     const { studentInfoLink, assessmentLink } = req.body;
     const spreadsheetId = getSpreadsheetIdFromLink(assessmentLink);
-    const client = new google.auth.JWT(keys.client_email, null, keys.private_key, ["https://www.googleapis.com/auth/spreadsheets"]);
+    const sheet = new google.auth.JWT(keys.client_email, null, keys.private_key, ["https://www.googleapis.com/auth/spreadsheets"]);
     const opt = { spreadsheetId, range: "A2:A" };
-    await client.authorize();
-    const gsapi = google.sheets({ version: "v4", auth: client });
+    await sheet.authorize();
+    const gsapi = google.sheets({ version: "v4", auth: sheet });
     const data = await gsapi.spreadsheets.values.get(opt);
     console.log(data.data.values);
-    const formsClient = new google.auth.JWT(keys.client_email, null, keys.private_key, ["https://www.googleapis.com/auth/forms"]);
-    await formsClient.authorize();
+    const questions = data.data.values;
+    const formUrl = await createGoogleForm(questions);    
+    res.json({ formUrl });
 
-    res.json({ studentInfoLink, assessmentLink, data });
+    res.json({ studentInfoLink, assessmentLink, data});
   }
   catch (error) {
     console.error(error);
@@ -47,6 +49,23 @@ app.post("/getData", async (req, res) => {
   }
 });
 
+async function createGoogleForm(questions) {
+  try {
+    const response = await fetch(googleScriptUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ questions })
+    });
+    
+    const { formUrl } = await response.json();
+    return formUrl;
+  } catch (error) {
+    console.error('Error creating Google Form:', error);
+    throw error;
+  }
+}
 
 const port = process.env.PORT || 8080;
-app.listen(port, () => console.log('Listening on port ${port}...'));
+app.listen(port, () => console.log(`Listening on port ${port}...`));
