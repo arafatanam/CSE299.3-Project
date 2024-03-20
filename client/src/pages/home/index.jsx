4a2f48a90e9334fdfade3af5242eba6e2d609e6c
Rajib Mohammed Nasim <rajib.nasim@northsouth.edu>
2024-03-20 13:46:47 +0600

    Added PDF Files Input

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -2,11 +2,14 @@ import React, { useState } from "react";
 import styles from "./styles.module.css";
 import axios from "axios";
 
+
 function Home(userDetails) {
     const user = userDetails.user;
     const [studentInfoLink, setStudentInfoLink] = useState("");
     const [assessmentLink, setAssessmentLink] = useState("");
     const [receivedLinks, setReceivedLinks] = useState(null);
+    const [pdfFile, setPdfFile] = useState(null);
+
 
     const handleSubmit = async () => {
         try {
@@ -26,17 +29,23 @@ function Home(userDetails) {
         }
     };
 
+
     const handleLogout = () => {
         window.open(`${process.env.REACT_APP_API_URL}/auth/logout`, "_self");
     };
+
+
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        setPdfFile(file);
+    };
+
 
     return (
         <div className={styles.container}>
             <h1 className={styles.heading}>Home</h1>
             <div className={styles.form_container}>
                 <div className={styles.content}>
-                <img src={user.picture} alt="profile" className={styles.profile_img}
-					/>
                     <div>
                         <p>Student Information Link</p>
                         <input
@@ -56,6 +65,15 @@ function Home(userDetails) {
                         />
                     </div>
                     <div>
+                        <p>Upload PDF File</p>
+                        <input
+                            type="file"
+                            accept=".pdf"
+                            onChange={handleFileChange}
+                            className={styles.input}
+                        />
+                    </div>
+                    <div>
                         <button className={styles.btn} onClick={handleSubmit}>
                             Submit
                         </button>
@@ -69,4 +87,10 @@ function Home(userDetails) {
     );
 }
 
+
 export default Home;
+
+
+
+
+
